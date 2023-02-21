c1987b71c860e4d400ddcd605ce553670b9597f9
Fijnanda van Klingeren <62397147+FijnandaVanKlingeren@users.noreply.github.com>
2023-02-21 14:31:51 +0100

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,11 @@
+
+RANDOM_SEED = 'abacadabra'
+TEMPLATE_FILE = 'DCE_-_template.qsf'
+ARRAY_FILE = 'array.csv'
+
+SURVEY_NAME = 'DCE - Voorbeeld'
+SURVEY_OWNER_ID = 'UR_cSyTmKmUm8P7wPP'
+SURVEY_CREATOR_ID = 'UR_cSyTmKmUm8P7wPP'
+
+MAIN_LABEL = 'Welk bedrijf heeft uw voorkeur?'
+COLUMN_LABELS = ['Bedrijf A', 'Bedrijf B']

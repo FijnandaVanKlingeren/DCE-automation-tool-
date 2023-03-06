18ceaec05de4b05527cc3348b9715e584b916fa7
Fijnanda van Klingeren <62397147+FijnandaVanKlingeren@users.noreply.github.com>
2023-03-06 10:14:05 +0100

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,8 +1,8 @@
 
 RANDOM_SEED = 'abacadabra'
 TEMPLATE_FILE = 'template_DCE_modest_border.qsf'
-ARRAY_FILE = 'dce_questions2.csv'  # set to None to skip the DCE questions
-EXTRA_FILE = 'survey_questions2.csv'  # set to None to skip the extra questions
+ARRAY_FILE = 'dce_questions.csv'  # set to None to skip the DCE questions
+EXTRA_FILE = 'survey_questions.csv'  # set to None to skip the extra questions
 
 SURVEY_NAME = 'DCE - Voorbeeld'
 SURVEY_OWNER_ID = 'UR_cSyTmKmUm8P7wPP'
